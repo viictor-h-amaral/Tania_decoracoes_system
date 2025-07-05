e8dff868e5c82b276e05aa52cb25cb91408afe3a
viictor-h-amaral <victorhamaralom@gmail.com>
2025-07-05 10:33:25 -0300

    efeito Botão selecionado adicionado e funcional

--- Sistema/dotnet/Solutions/TaniaDecoracoesSystem/TaniaDecoracoes.WPFLibrary/ViewModel/UserControl/MenuModulosViewModel.cs
+++ Sistema/dotnet/Solutions/TaniaDecoracoesSystem/TaniaDecoracoes.WPFLibrary/ViewModel/UserControl/MenuModulosViewModel.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TaniaDecoracoes.WPFLibrary.Utils;
 
 namespace TaniaDecoracoes.WPFLibrary.ViewModel.UserControl
 {
     public class MenuModulosViewModel :ViewModelBase
     {
+        private string _moduloSelecionado = "_ModuloHome";
+        public string ModuloSelecionado
+        {
+            get => _moduloSelecionado;
+            set => SetProperty(ref _moduloSelecionado, value);
+        }
 
+        public ICommand SelecionarModuloCommand { get; }
+        public MenuModulosViewModel()
+        {
+            SelecionarModuloCommand = new RelayCommand<string>(modulo => 
+            {
+                ModuloSelecionado = modulo; 
+            });
+        }
     }
 }

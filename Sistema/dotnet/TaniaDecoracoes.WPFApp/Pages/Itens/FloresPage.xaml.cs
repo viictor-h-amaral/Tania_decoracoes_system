d7203244209585e05e34af4b52bc6eadb5dbde48
viictor-h-amaral <victorhamaralom@gmail.com>
2025-10-03 20:57:06 -0300

    adicionada página de flores

---
+++ Sistema/dotnet/TaniaDecoracoes.WPFApp/Pages/Itens/FloresPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaniaDecoracoes.WPFApp.ViewModel.Pages.Itens;
+
+namespace TaniaDecoracoes.WPFApp.Pages.Itens
+{
+    /// <summary>
+    /// Interação lógica para FloresPage.xam
+    /// </summary>
+    public partial class FloresPage : Page
+    {
+        public FloresPage()
+        {
+            InitializeComponent();
+
+            var vm = new FloresPageViewModel();
+            this.DataContext = vm;
+        }
+    }
+}
---
+++ Sistema/dotnet/TaniaDecoracoes.WPFApp/ViewModel/Pages/Itens/FloresPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using TaniaDecoracoes.Entities.Models.Itens;
+using TaniaDecoracoes.WPFLibrary.Utils.GridUtils;
+using TaniaDecoracoes.WPFLibrary.ViewModel;
+using TaniaDecoracoes.WPFLibrary.ViewModel.Interfaces;
+using TaniaDecoracoes.WPFLibrary.ViewModel.UserControl;
+
+namespace TaniaDecoracoes.WPFApp.ViewModel.Pages.Itens
+{
+    public class FloresPageViewModel : ViewModelBase
+    {
+        private IGridViewModel? _dataGridVM;
+        public IGridViewModel? DataGridVM
+        {
+            get => _dataGridVM;
+            set => SetProperty(ref _dataGridVM, value);
+        }
+        public FloresPageViewModel()
+        {
+            var gridConfig = new GridConfigObject(title: "Flores",
+                                                    readOnly: true,
+                                                    autoGenerateColumns: true,
+                                                    (DefaultActionButtons.All));
+
+            DataGridVM = new CommonDataGridViewModel<Item>(gridConfig) { };
+
+            DataGridVM.AddDefaultTableButtons();
+        }
+    }
+}
